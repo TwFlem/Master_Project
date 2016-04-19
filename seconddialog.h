38b6649ca47a0ba4c0000b47371111e78c0607f6
TwFlem <twfleming1@ufl.edu>
2016-04-19 18:29:15 -0400

    Update seconddialog.h

--- seconddialog.h
+++ seconddialog.h
@@ -5,7 +5,7 @@
 #include <QDialog>
 
 namespace Ui {
-class SecondDialog;
+class SecondDialog; //the second dialog window
 }
 
 class SecondDialog : public QDialog
@@ -17,11 +17,11 @@ public:
     ~SecondDialog();
 
 private slots:
-    void on_pushButton_clicked();
+    void on_pushButton_clicked(); //button corresponding to verify user
 
 private:
     Ui::SecondDialog *ui;
-    void verifyUser(QString name);
+    void verifyUser(QString name);  //veryfies user
 
 };
 
