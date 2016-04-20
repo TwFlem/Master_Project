94ac1483ce3cc87f556edb7f6e48e76a686975da
TwFlem <twfleming1@ufl.edu>
2016-04-20 02:49:00 -0400

    Update help.cpp

--- help.cpp
+++ help.cpp
@@ -1,14 +1,14 @@
 #include "help.h"
 #include "ui_help.h"
 
-help::help(QWidget *parent) :
+help::help(QWidget *parent) : //help gui constructor
     QDialog(parent),
     ui(new Ui::help)
 {
     ui->setupUi(this);
 
     QFile file("C:\\Users\\Byron\\Desktop\\C++\\QT Stuff\\build-QT_APP-Desktop_Qt_5_6_0_MSVC2015_64bit-Debug\\instructions.txt");
-    if(file.open(QIODevice::ReadOnly))
+    if(file.open(QIODevice::ReadOnly)) //uploads prewritten help dialog
     {
         QTextStream in(&file);
         ui->textBrowser->setText(in.readAll());
@@ -20,6 +20,7 @@ help::~help()
     delete ui;
 }
 
+//displays help information when button is clicked
 void help::on_pushButton_clicked()
 {
     close();
