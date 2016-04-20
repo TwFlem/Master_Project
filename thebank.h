66413d3d144609a0fd1be41eb97e9ba069b229d9
TwFlem <twfleming1@ufl.edu>
2016-04-20 01:24:01 -0400

    Update thebank.h

--- thebank.h
+++ thebank.h
@@ -8,32 +8,32 @@ namespace Ui {
 class theBank;
 }
 
-class theBank : public QDialog
+class theBank : public QDialog //bank gui
 {
     Q_OBJECT
 
 public:
     explicit theBank(QWidget *parent = 0);
-    theBank(int tokens);
-    theBank(int tokens, QString name);
+    theBank(int tokens);                    //bank tokens constructor
+    theBank(int tokens, QString name);      //bank token and username constructor
     ~theBank();
-    int getNumTokens();
-    void setNumTokens(int newNumTokens);
-    void addNumTokens(int newNumTokens);
-    bool enoughTokens(int numNeeded);
-    void subtractNumTokens(int numNeeded);
+    int getNumTokens();                     //returns number of tokens
+    void setNumTokens(int newNumTokens);    //sets number of tokens
+    void addNumTokens(int newNumTokens);    //adds number of tokens
+    bool enoughTokens(int numNeeded);       //determines if there are enough tokens
+    void subtractNumTokens(int numNeeded);  //subtracts number of tokens after purchase
 
 private slots:
-    void on_pushButton_clicked();
+    void on_pushButton_clicked();           //displays bank account when pushed
 
 private:
-    Ui::theBank *ui;
-    QMovie * movie;
-    int tokenNumber;
-    QString convertToString(int x);
-    void setName(QString user);
-    QString getName();
-    QString name;
+    Ui::theBank *ui;                        //bank's ui
+    QMovie * movie;                         //bank's animaion
+    int tokenNumber;                        //number of tokens in bank
+    QString convertToString(int x);         //convert int to string
+    void setName(QString user);             //set name to username as desired
+    QString getName();                      //returns username
+    QString name;                           //username
 
 };
 
