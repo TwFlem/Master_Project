5a077305040d0b41592dd2176ff8c734e97c9858
TwFlem <twfleming1@ufl.edu>
2016-04-20 02:43:06 -0400

    Update createnewplayer.cpp

--- createnewplayer.cpp
+++ createnewplayer.cpp
@@ -7,20 +7,20 @@
 #include <QtDebug>
 #include <QFileInfo>
 
-createNewPlayer::createNewPlayer(QWidget *parent) :
+createNewPlayer::createNewPlayer(QWidget *parent) : //Create new player constructor
     QDialog(parent),
     ui(new Ui::createNewPlayer)
 {
-    ui->setupUi(this);
-    setWindowTitle(tr("New Player Creation"));
+    ui->setupUi(this);  //creates gui
+    setWindowTitle(tr("New Player Creation"));  //sets gui title
 }
 
-createNewPlayer::~createNewPlayer()
+createNewPlayer::~createNewPlayer()//destructor
 {
     delete ui;
 }
 
-void createNewPlayer::on_pushButton_clicked()
+void createNewPlayer::on_pushButton_clicked()   //when clicked...
 {
     //set Username for player
    QString name =  ui->lineEdit->text();
@@ -30,27 +30,29 @@ void createNewPlayer::on_pushButton_clicked()
 
 }
 
-void createNewPlayer::createPlayerFile(QString name)
+void createNewPlayer::createPlayerFile(QString name)    //create new player with designated name
 {
-    setPlayerName(name);
-    QFile playerFile(name+".txt");
+    setPlayerName(name);            //set's player with desired name
+    QFile playerFile(name+".txt");  //reads player file
     qDebug() << name;
 
-    if(playerFile.open(QIODevice::ReadOnly | QIODevice::Text))
+    if(playerFile.open(QIODevice::ReadOnly | QIODevice::Text))  //sends error because username already exists
     {
         QMessageBox::warning(this, tr("Player Creation Error"), tr("User with this name already exist please choose a different name"));
         playerFile.close();
         return;
     }
     else{
-        if(playerFile.open(QIODevice::WriteOnly | QIODevice::Text))
+        if(playerFile.open(QIODevice::WriteOnly | QIODevice::Text)) //allows new player to be created
         {
-
+            
+            //writes to new file
             QTextStream text(&playerFile);
             text << name << "\n" << "100" <<  "\n";
             text.flush();
             playerFile.close();
-
+            
+            //alerts user that username has been created
             QMessageBox::information(this, tr("Player Creation Successful!"), tr("Welcome to the game! Your text file has been created you may exit."));
             newPetWindow = new createNewPet(getPlayerName());
             newPetWindow->show();
@@ -64,7 +66,7 @@ void createNewPlayer::createPlayerFile(QString name)
 
 }
 
-bool createNewPlayer::checkifFileExists(QString file)
+bool createNewPlayer::checkifFileExists(QString file) //checks to see if username already exists
 {
 
     QFileInfo check_file(file);
@@ -79,12 +81,12 @@ bool createNewPlayer::checkifFileExists(QString file)
     }
 }
 
-void createNewPlayer::setPlayerName(QString name)
+void createNewPlayer::setPlayerName(QString name)   //set's player name to file
 {
     this->player = name;
 }
 
-QString createNewPlayer::getPlayerName()
+QString createNewPlayer::getPlayerName()    //returns username
 {
     return player;
 }
